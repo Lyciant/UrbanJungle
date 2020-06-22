202934227fac06e8f878717ad5e2d72a424e1679
Lyciant <Lyciant.em@gmail.com>
2020-06-22 15:04:05 +0200

    Adding Original Files
    Adding Original Files and creating the Master Branch

---
+++ Source/UrbanJungle/MonkeyBoyPlayerCharacter.cpp
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MonkeyBoyPlayerCharacter.h"
+
+// Sets default values
+AMonkeyBoyPlayerCharacter::AMonkeyBoyPlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Set player0 to controll this class by default
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+
+}
+
+// Called when the game starts or when spawned
+void AMonkeyBoyPlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+
+	
+}
+
+// Called every frame
+void AMonkeyBoyPlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	Internal_AddMovementInput(FVector(ConstantMovementForce, 0.0f, 0.0f));
+}
+
+// Called to bind functionality to input
+void AMonkeyBoyPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	// Setting up Action Bindings
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AMonkeyBoyPlayerCharacter::StartJump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AMonkeyBoyPlayerCharacter::StopJump);
+}
+
+
+// Set jump flag when key pressed
+void AMonkeyBoyPlayerCharacter::StartJump()
+{
+	bPressedJump = true;
+}
+
+// Clear jump flag when key released
+void AMonkeyBoyPlayerCharacter::StopJump()
+{
+	bPressedJump = false;
+}
+
---
+++ Source/UrbanJungle/MonkeyBoyPlayerCharacter.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MonkeyBoyPlayerCharacter.generated.h"
+
+UCLASS()
+class URBANJUNGLE_API AMonkeyBoyPlayerCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AMonkeyBoyPlayerCharacter();
+
+	UPROPERTY(EditAnywhere)
+	float ConstantMovementForce = 200.0f;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Set jumping flag on key pressed
+	UFUNCTION()
+	void StartJump();
+
+	// Clear jumping flag on key release
+	UFUNCTION()
+	void StopJump();
+};
